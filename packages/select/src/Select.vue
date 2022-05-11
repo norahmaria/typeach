5d939d071aabe83e683ccb0dff05e10557f22c75
Norah Maria Hareide Nilsen <norahmariano@gmail.com>
2022-05-11 08:21:40 +0200

    Switched Select file

---
+++ packages/select/src/Select.vue
@@ -0,0 +1,32 @@
+<template>
+  <button class="lv-button">
+    <h2>{{ title }}</h2>
+    <slot />
+    {{ snowflake }}
+  </button>
+</template>
+
+<script setup lang="ts">
+  defineProps({
+    title: {
+      type: String,
+      default: '',
+    },
+
+    snowflake: {
+      type: String,
+      required: true,
+    },
+  })
+</script>
+
+<style lang="scss" scoped>
+  .lv-button {
+    background-color: red;
+    font-size: 40px;
+
+    h2 {
+      color: #ff0;
+    }
+  }
+</style>
