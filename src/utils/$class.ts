91d69394eeb69d1540c66bc51e0a57f7aa151533
Norah Maria Hareide Nilsen <norahmariano@gmail.com>
2022-06-08 08:55:04 +0200

    Add string options to class utility

--- src/utils/$class.ts
+++ src/utils/$class.ts
@@ -1,32 +1,52 @@
 import { getCurrentInstance } from 'vue'
 
-interface iClass {
+interface options {
+  [key: string]: boolean | string
+}
+
+interface Class {
   [key: string]: boolean
 }
 
 const toKebabCase = (name: string): string =>
   name.replace(/([A-Za-z])([A-Z])/g, '$1-$2').toLowerCase()
 
-const filterToStrings = (element: string | iClass): element is string =>
+const filterToStrings = (element: string | options): element is string =>
   typeof element === 'string'
 
-const filterToObjects = (element: string | iClass): element is iClass =>
+const filterToObjects = (element: string | options): element is options =>
   typeof element === 'object'
 
-const getClassObject = (classes: string[], options: iClass): iClass => {
+const isString = (element: string | boolean): element is string =>
+  typeof element === 'string'
+
+const isBoolean = (element: string | boolean): element is boolean =>
+  typeof element === 'boolean'
+
+const getClassObject = (classes: string[], options: options): Class => {
   const name = classes.join('__')
 
   const object = { [name]: true }
 
   for (const key of Object.keys(options)) {
-    object[`${name}--${key}`] = options[key]
+    const value = options[key]
+
+    // variant class
+    if (isString(value)) {
+      object[`${name}--${value}`] = true
+    }
+
+    // conditional class
+    if (isBoolean(value)) {
+      object[`${name}--${key}`] = value
+    }
   }
 
   return object
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const $class = (...classes: (string | iClass)[]) => {
+export const $class = (...classes: (string | options)[]) => {
   const component = getCurrentInstance()
 
   const name = component?.type?.name ?? 'no-name'
