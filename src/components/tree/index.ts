aec930497bcf8ad9e965ac163dcc5e79388a9764
Norah Maria Hareide Nilsen <norahmariano@gmail.com>
2024-06-25 15:12:15 +0200

    Export Tree types

--- src/components/tree/index.ts
+++ src/components/tree/index.ts
@@ -3,6 +3,9 @@ import Item from "./TreeItem.vue";
 import ItemLabel from "./TreeItemLabel.vue";
 import ItemIndicator from "./TreeItemIndicator.vue";
 
+export type { TreeProps } from "./Tree.vue";
+export type { TreeItemProps } from "./TreeItem.vue";
+
 export const PeachyTree = {
   Tree,
   Item,
